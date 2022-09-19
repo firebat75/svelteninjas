cac180207da3a3d3f8f5c8ceb52e9f75173b2d7b
firebat75 <jacklowry1337@gmail.com>
2022-09-18 21:07:19 -0400

    added Modal

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,6 @@
 <script>
+	import Modal from "./Modal.svelte";
+
 	let people = [
 		{ name: "yoshi", beltColour: "black", age: 25, id: 1 },
 		{ name: "mario", beltColour: "orange", age: 45, id: 2 },
@@ -9,12 +11,27 @@
 		people = people.filter((person) => person.id != id);
 		console.log(e);
 	};
+
+	let num = 5;
 </script>
+
+<Modal />
+
+{#if num > 20}
+	<p>Greater than 20</p>
+{:else if num > 5}
+	<p>Greater than 5</p>
+{:else}
+	<p>Not greater than 5</p>
+{/if}
 
 <main>
 	{#each people as person (person.id)}
 		<div>
 			<h4>{person.name}</h4>
+			{#if person.beltColour === "black"}
+				<p><strong>MASTER NINJA</strong></p>
+			{/if}
 			<p>{person.age} years old, {person.beltColour} belt</p>
 			<button
 				on:click={(e) => {
